bc14e61f2cc75ca5f989fa65e1c7344ddc4bea75
dadaxingxing <96450279+dadaxingxing@users.noreply.github.com>
2025-05-22 19:29:44 -0700

    attempted at getting screenshots of images and questions

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from dotenv import load_dotenv
 import mss 
 import mss.tools
 import keyboard
+import pyautogui
+
 
 load_dotenv()
 
@@ -11,19 +13,33 @@ API_key = os.getenv('API_KEY')
 client = genai.Client(api_key=API_key)
 
 def kahoot_bot():
-        
+
+    question_and_answers = {
+        0: (0.0, 0.072, 1.0, 0.2),
+        1: (0.042, 0.667, 0.458, 0.742),
+        2: (0.542, 0.667, 1.0, 0.742),
+        3: (0.042, 0.8, 0.458, 0.875),
+        4: (0.542, 0.8, 1.0, 0.875),
+    }
+    
+    width, height = pyautogui.size()
     with mss.mss() as sct:
+        
+        for key, value in question_and_answers.items():
+            
+            x1, y1, x2, y2 = value[0] * width, value[1] * height, value[2] * width, value[3] * height
+            
 
-        # The screen part to capture
-        monitor = {"top": 160, "left": 160, "width": 160, "height": 135}
-        output = "window.png"
+            # The screen part to capture
+            monitor = {"top": y1, "left": x1, "width": y2 - y1, "height": x2 - x1}
+            output = "window.png"
 
-        # Grab the data
-        sct_img = sct.grab(monitor)
+            # Grab the data
+            sct_img = sct.grab(monitor)
 
-        # Save to the picture file
-        mss.tools.to_png(sct_img.rgb, sct_img.size, output=output)
-        print(output)
+            # Save to the picture file
+            mss.tools.to_png(sct_img.rgb, sct_img.size, output=output)
+            print(output)
 
 
 
@@ -32,5 +48,5 @@ def kahoot_bot():
 # )
 # print(response.text)
 
-keyboard.add_hotkey('ctrl+shift+s', kahoot_bot)
+keyboard.add_hotkey('ctrl+shift+t', kahoot_bot)
 keyboard.wait()
